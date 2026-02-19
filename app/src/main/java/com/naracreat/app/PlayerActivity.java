a68337fc3dc70c2548ee272408495d9f7e587784
naracreat <narablewer@gmail.com>
2026-02-20 02:06:42 +0700

    fix PlayerActivity extends AppCompatActivity + media3 PlayerView layout

--- app/src/main/java/com/naracreat/app/PlayerActivity.java
+++ app/src/main/java/com/naracreat/app/PlayerActivity.java
@@ -1,13 +1,44 @@
 package com.naracreat.app;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.media3.common.MediaItem;
+import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.ui.PlayerView;
+
 public class PlayerActivity extends AppCompatActivity {
+
+    private ExoPlayer player;
+    private PlayerView playerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        playerView = findViewById(R.id.player_view);
+
+        player = new ExoPlayer.Builder(this).build();
+        playerView.setPlayer(player);
+
+        String url = getIntent().getStringExtra("url");
+        if (url == null) url = "";
+
+        MediaItem item = MediaItem.fromUri(Uri.parse(url));
+        player.setMediaItem(item);
+        player.prepare();
+        player.play();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (player != null) {
+            player.release();
+            player = null;
+        }
     }
 }
