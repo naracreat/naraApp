c818abc1eebaed48a730b249717607db448d4aac
naracreat <suprablewer@gmail.com>
2026-02-20 05:26:03 +0700

    Rename PostResponse class to PostsResponse

--- app/src/main/java/com/naracreat/app/PostResponse.java
+++ app/src/main/java/com/naracreat/app/PostResponse.java
@@ -2,7 +2,7 @@ package com.naracreat.app;
 
 import java.util.List;
 
-public class PostResponse {
+public class PostsResponse {
     public int page;
     public int totalPages;
     public int total;
