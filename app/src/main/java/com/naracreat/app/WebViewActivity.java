900d721ee1ff6de49b3018f47133fc07b4f4dbd5
naracreat <suprablewer@gmail.com>
2026-02-20 04:33:57 +0700

    Create WebViewActivity.java

---
+++ app/src/main/java/com/naracreat/app/WebViewActivity.java
@@ -0,0 +1,39 @@
+package com.naracreat.app;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        String url = getIntent().getStringExtra("url");
+        if (url == null) url = "https://saweria.co/Narapoi";
+
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setLoadWithOverviewMode(true);
+        s.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView w = (WebView) findViewById(android.R.id.content).getRootView();
+        super.onBackPressed();
+    }
+}
