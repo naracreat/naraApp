116fe53d5cc9f35d1d00c128ad3fdbd1d6072a72
naracreat <suprablewer@gmail.com>
2026-02-20 13:09:00 +0700

    Update and rename PostResponse.java to PostsResponse.java

--- app/src/main/java/com/naracreat/app/PostsResponse.java
+++ app/src/main/java/com/naracreat/app/PostsResponse.java
@@ -3,7 +3,7 @@ package com.naracreat.app;
 import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
-public class PostResponse {
+public class PostsResponse {
 
     @SerializedName("page")
     public int page;
