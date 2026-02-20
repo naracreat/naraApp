2b935f95771f28dcb131abd422305cd4963070ea
naracreat <suprablewer@gmail.com>
2026-02-20 13:02:10 +0700

    Update ApiService.java

--- app/src/main/java/com/naracreat/app/ApiService.java
+++ app/src/main/java/com/naracreat/app/ApiService.java
@@ -6,7 +6,11 @@ import retrofit2.http.Query;
 
 public interface ApiService {
 
+    // bisa dipanggil tanpa argumen: api.getPosts()
+    @GET("api/posts")
+    Call<PostResponse> getPosts();
+
+    // bisa dipanggil pakai page: api.getPosts(1)
     @GET("api/posts")
     Call<PostResponse> getPosts(@Query("page") int page);
-
 }
