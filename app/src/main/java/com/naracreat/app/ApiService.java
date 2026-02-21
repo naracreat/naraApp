d613c7113e7ce0c926ccb95a7741d285feabdc51
naracreat <narablewer@gmail.com>
2026-02-21 15:04:16 +0700

    refine player

--- app/src/main/java/com/naracreat/app/ApiService.java
+++ app/src/main/java/com/naracreat/app/ApiService.java
@@ -6,7 +6,15 @@ import retrofit2.http.Query;
 
 public interface ApiService {
 
+    // https://narahentai.pages.dev/api/posts?page=1
     @GET("api/posts")
     Call<PostResponse> getPosts(@Query("page") int page);
 
+    // Kalau nanti kamu tambah search/genre di backend, baru aktifin ini:
+    // @GET("api/posts")
+    // Call<PostResponse> posts(
+    //         @Query("page") int page,
+    //         @Query("q") String q,
+    //         @Query("genre") String genre
+    // );
 }
